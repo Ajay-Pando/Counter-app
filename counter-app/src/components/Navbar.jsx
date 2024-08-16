4fbc3e976a4392e2e33f3216f9f4134b29b7213a
Ajay-Pando <ajay.kumar@pandoconsultants.com>
2024-08-16 10:49:24 +0530

    Counter app

--- counter-app/src/components/Navbar.jsx
+++ counter-app/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <div className="bg-blue-950 text-white text-center h-14 place-content-center">
+      <div className="bg-blue-950 text-white text-center h-14 place-content-center ">
         <h1 className="font-bold text-xl">Counter App using Tailwind</h1>
       </div>
     </>
